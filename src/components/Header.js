20e6181beee0bc2bb75ed98cd1c0560339fcdbd6
fespinosa <fjevictoriano@yahoo.com>
2021-08-12 18:02:42 -0400

    Move header content to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,20 @@
 const Header = () => {
-    return ( <dvi></dvi> );
-}
- 
+  return (
+    <header>
+      <div class="grid grid-cols-2">
+        <h1>
+          <span>
+            <a href="/">
+              fespinosa.<span>dev</span>
+            </a>
+          </span>
+        </h1>
+        <p>
+          writing by <a href="#">Fernando Espinosa</a>
+        </p>
+      </div>
+    </header>
+  );
+};
+
 export default Header;
