1c55bf0f3da4992206f5c93a26f17fea7baff2db
fespinosa <fjevictoriano@yahoo.com>
2021-08-12 18:01:21 -0400

    Arrange header content

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ function App() {
   return (
     <div className="content">
       <header>
-        <h2>
-          <span>
-            <a href="/">
-              fespinosa.<span>dev</span>
-            </a>
-          </span>
-        </h2>
-        <p>
-          writing by{" "}
-          <a href="https://twitter.com/orderedlist">Fernando Espinosa</a>
-        </p>
+        <div class="grid grid-cols-2">
+          <h1>
+            <span>
+              <a href="/">
+                fespinosa.<span>dev</span>
+              </a>
+            </span>
+          </h1>
+          <p>
+            writing by <a href="#">Fernando Espinosa</a>
+          </p>
+        </div>
       </header>
     </div>
   );
