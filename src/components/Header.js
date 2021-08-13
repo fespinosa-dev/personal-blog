932555853089b41d72ed30d9589042cb38b08c8c
fespinosa <fjevictoriano@yahoo.com>
2021-08-13 17:25:21 -0400

    Format header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,24 @@ import Social from "./social";
 const Header = () => {
   return (
     <header>
-      <div className="grid grid-rows-3">
+      <div className="grid grid-rows-3 gap-2">
         <div className="grid grid-cols-2">
           <h1>
             <span>
-              <a href="/">
-                fespinosa.<span>dev</span>
+              <a className="font-bold" href="/">
+                fespinosa.<span className="text-blue-1000">dev</span>
               </a>
             </span>
           </h1>
-          <p>
-            writing by <a href="#">Fernando Espinosa</a>
+          <p className="text-gray-600">
+            writing by{" "}
+            <a className="text-blue-1000 font-sans" href="#">
+              Fernando Espinosa
+            </a>
           </p>
         </div>
-        <div>
-          <p>Welcom to my blog!</p>
+        <div className="text-gray-700">
+          <p>“Simplicity is the soul of efficiency.” – Austin Freeman</p>
         </div>
         <div>
           <Social website={"#"} github={"#"} twitter={"#"} linkedin={"#"} />
