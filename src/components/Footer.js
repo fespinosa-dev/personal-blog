2530448c1986d469ceb9c247b89e61eb0dd7df4e
fespinosa-dev <fespinosa-dev@users.noreply.github.com>
2021-08-14 15:46:23 +0000

    deploy: 8aa71bbb274825db188906138821edb550f7eb3b

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer>
-      <p>Copyright © 2021 Fernando Espinosa. All Rights Reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
--- src/components/social/Wrapper.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-  margin-top: 10px;
-  text-align: center;
-`;
-
-export default Wrapper;
